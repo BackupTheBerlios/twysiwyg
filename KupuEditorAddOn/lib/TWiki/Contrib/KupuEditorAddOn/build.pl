1302d105af7c2ec32aaea99363ccf45e2a9aceeb
romano <romano>
2004-12-18 17:34:57 +0000

    1.0b5 with server-side Perl translator - manifest creation and filter updated

--- KupuEditorAddOn/lib/TWiki/Contrib/KupuEditorAddOn/build.pl
+++ KupuEditorAddOn/lib/TWiki/Contrib/KupuEditorAddOn/build.pl
@@ -1,21 +1,4 @@
 #!/usr/bin/perl -w
-#
-# Example build class. Copy this file to the equivalent place in your
-# plugin or contrib and edit.
-#
-# Read the comments at the top of lib/TWiki/Contrib/Build.pm for
-# details of how the build process works, and what files you
-# have to provide and where.
-#
-# Requires the environment variable TWIKI_LIBS (a colon-separated path
-# list) to be set to point at the build system and any required dependencies.
-# Usage: ./build.pl [-n] [-v] [target]
-# where [target] is the optional build target (build, test,
-# install, release, uninstall), test is the default.`
-# Two command-line options are supported:
-# -n Don't actually do anything, just print commands
-# -v Be verbose
-#
 
 # Standard preamble
 BEGIN {
@@ -40,13 +23,13 @@ use TWiki::Contrib::Build;
     my $this = shift;
     my $basedir = $TWiki::Contrib::Build::basedir;
     chdir("$basedir") || die "can't cd to $basedir - $!";
-    print `find . -type f | grep -v CVS | egrep -v '~\$' | egrep "([kK]upu|LICENSE*)" > $basedir/MANIFEST`;
+    print `find . -type f | grep -v CVS | egrep -v '~\$' | egrep "([kK]upu|LICENSE*|Twiki*)" > $basedir/MANIFEST`;
   }
   
   sub filter {
     my $this = shift;
     my ( $from, $to ) = @_;
-    return $this->SUPER::filter( $from, $to ) if ( $from =~ m/[a-zA-Z0-9_]+\.pl/ || $from =~ m/KupuEditorAddOn\.txt/ );
+    return $this->SUPER::filter( $from, $to ) if ( $from =~ m/KupuEditorAddOn_installer\.pl/ || $from =~ m/KupuEditorAddOn\.txt/ );
   }
 
 }
